bd67317b2086f22b3957c18c9d200914654db978
IvanDF <delfatti.ivan@gmail.com>
2021-05-10 22:33:12 +0200

    creating ui components

---
+++ src/packages/ui/components/ThemeComponent/Theme.js
@@ -0,0 +1,41 @@
+export const Theme = {
+  colors: {
+    //Color HEX
+    black: "#000000",
+    whithe: "#ffffff",
+    primmary: "#EAE2B7",
+    cinfitrm: "#FCBF49",
+    warnng: "#F77F00",
+    danger: "#D62828",
+    bg: "#003049",
+  },
+
+  opacity: {
+    //Opacity values
+    o9: 0.9,
+    o8: 0.8,
+    o7: 0.7,
+    oo5: 0.05,
+    o6: 0.6,
+    o5: 0.5,
+    o4: 0.4,
+    o3: 0.3,
+    o2: 0.2,
+    o1: 0.1,
+    o0: 0,
+  },
+
+  font: {
+    //Size
+    s12: "12px",
+    s14: "14px",
+    s16: "16px",
+    s20: "20px",
+    s24: "24px",
+    s34: "34px",
+
+    //Weight
+    w400: 400,
+    w500: 500,
+  },
+};
---
+++ src/packages/ui/index.js
@@ -0,0 +1,6 @@
+export * from "./components/IconComponent/IconComponent";
+export * from "./components/ThemeComponent/Theme";
+// export * from "./Components/ImageComponent/ImageComponent";
+// export * from "./Components/InputComponent/InputComponent";
+// export * from "./Components/TypographyComponent/TypographyComponent";
+//
